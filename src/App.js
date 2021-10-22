11f56c05410e8d7ad7659c400c1fe6e538eda5bb
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-10-22 08:35:38 -0300

    modify Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Tasks from "./components/Tasks"
+import './App.css'
+
+const App = () => {
+    const [tasks, setTasks] = useState([
+      {
+        id: '1',
+        title: 'Estudar programação',
+        completed: false,
+      }, 
+      {
+        id: '2',
+        title: 'Ler Livros',
+        completed: true,
+      },
+  ]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className='container'></div>
+        
+    </>
   );
-}
+  
+};
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const tasks = () => {
+    return <h1>Tasks</h1>;
+};
+
+export default tasks;
