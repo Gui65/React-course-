e65c407f4f7d3a2cb11bcc60b1dc83ae6af275eb
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-10-22 09:53:34 -0300

    update AddTask and AddTask.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Tasks from "./components/Tasks"
 import './App.css'
+import AddTask from "./components/AddTask";
 
 const App = () => {
     const [tasks, setTasks] = useState([
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <>
         <div className='container'>
+              <AddTask />
               <Tasks tasks={tasks} />
         </div>
           
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './AddTask.css';
+
+const AddTask = () =>{
+    return(
+        <div className="add-task-container">
+            <input className="add-task-input" type="text" />;
+
+        </div>
+    );
+};
+
+export default AddTask;
