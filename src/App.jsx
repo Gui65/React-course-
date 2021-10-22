cbdb17a99fedd0247f2ef742b9c684703b6e3a8d
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-10-22 16:27:57 -0300

    add button delete and mark as done

--- src/App.jsx
+++ src/App.jsx
@@ -37,13 +37,22 @@ const App = () => {
 
 
     setTasks(newTasks);
+  };
+
+  const handleTaskDeletion = (taskId) => {
+      const newTasks = tasks.filter(task => task.id !== taskId)
+
+      setTasks(newTasks);
   }
   
   return (
     <>
         <div className='container'>
               <AddTask handleTaskAddition={handleTaskAddition}/>
-              <Tasks tasks={tasks}  handleTaskClick={handleTaskClick}/>
+              <Tasks 
+                  tasks={tasks}
+                  handleTaskClick={handleTaskClick}
+                  handleTaskDeletion={handleTaskDeletion} />
         </div>
           
     </>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 import "./Task.css";
 
-const Task = ({ task, handleTaskClick }) => {
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
     return(
-        <div className="task-container" style={task.completed ? {borderLeft: "6px solid chartreuse"} : {}}>
-            <div className="task-title" onClick={handleTaskClick}>
+        <div 
+            className="task-container"
+            style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+        >
+
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
                 {task.title}
+            </div>
+
+            <div className="buttons-container">
+                <button className="remove-task-button"
+                onClick={() => handleTaskDeletion(task.id)}>
+                    X
+                </button>
             </div>
         </div>
     );
-    //return <div className="task-container">{task.title}</div>;
+   
 };
 
 export default Task;
