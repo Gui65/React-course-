f97320db7bfe3ea9cee390704b2c29a233017f3c
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-10-27 11:10:05 -0300

    finishing

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,6 +8,7 @@ const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
         <>
             {tasks.map((task) => (
                 <Task
+                    key={task.id}
                     task={task}
                     handleTaskClick={handleTaskClick}
                     handleTaskDeletion={handleTaskDeletion}
